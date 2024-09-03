383dc1bef85ea940a29bdac9a6b957bb6baf2e87
S3b2 <111647190+Shaina-08@users.noreply.github.com>
2024-09-03 19:19:22 +0530

    SignUpForm and LoginForm

--- spamfinder/api/forms.py
+++ spamfinder/api/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from .models import User
-
+# Classes for Signup and Login forms  
 class SignupForm(UserCreationForm):
     phone_number = forms.CharField(max_length=15, required=True)
     name = forms.CharField(max_length=100, required=True)
